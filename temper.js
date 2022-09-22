ed5abf33b9eaf4c7eb50760a358b08fcc46db18f
Msoto24 <soto24.2000@gmail.com>
2022-09-22 10:03:36 -0500

    terminado

---
+++ temper.js
@@ -0,0 +1,29 @@
+let elForm = document.querySelector(".derecha");
+let elFormInput = document.querySelector(".form__input");
+let elFormInputLast = document.querySelector(".form__input-last");
+let eltemp=document.querySelector(".izquierda");
+
+
+
+elForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+
+  let inputValue = elFormInput.value;
+
+  let result = ((inputValue - 32) * 5/9).toFixed(2);
+
+  elFormInputLast.textContent = `${result}º`;
+ 
+    var element = document.getElementById('medidor')
+    element.style.height= `${result}px`; 
+    var element2 = document.getElementById('medidor')
+    element2.textContent= `${result}º`; 
+
+});
+function validar(){
+  formulario.reset();
+}
+validar()
+
+
+
